11ac7e60604d949d287ffe89d56c4dc4262fdf56
Marta Żak <martysik19@gmail.com>
2024-09-03 21:37:11 +0200

    Adds form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
 // write code here
+const refs = {
+  inputs: [...document.querySelectorAll('input')],
+  fields: [...document.querySelectorAll('fields')],
+};
+
+for (const input of refs.inputs) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+  input.placeholder = input.name;
+
+  input.closest('.field').appendChild(label);
+}
